2e9dd6287ce2b4815680aba3c7da4b0890e8479e
mahmud <mahmudmodibbo21@gmail.com>
2025-03-25 03:55:53 +0100

    using fraps charts and making good progress

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,11 @@
 <x-layouts.app :title="__('Dashboard')">
+
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <div class="grid auto-rows-min gap-4 md:grid-cols-3">
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-                <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-                <p> hey </p>
+                <div id="chart"></div>
+
+
             </div>
             <div class="relative aspect-video overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
                 <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
@@ -16,4 +18,31 @@
             <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
         </div>
     </div>
+    <script>
+        const data = {
+            labels: ["12am-3am", "3am-6pm", "6am-9am", "9am-12am",
+                "12pm-3pm", "3pm-6pm", "6pm-9pm", "9am-12am"
+            ],
+            datasets: [
+                {
+                    name: "Some Data", type: "bar",
+                    values: [25, 40, 30, 35, 8, 52, 17, -4]
+                },
+                {
+                    name: "Another Set", type: "line",
+                    values: [25, 50, -10, 15, 18, 32, 27, 14]
+                }
+            ]
+        }
+
+        const chart = new frappe.Chart("#chart", {  // or a DOM element,
+            // new Chart() in case of ES6 module with above usage
+            title: "My Awesome Chart",
+            data: data,
+            type: 'bar', // or 'bar', 'line', 'scatter', 'pie', 'percentage'
+            height: 250,
+            colors: ['#7cd6fd', '#743ee2']
+        })
+    </script>
+
 </x-layouts.app>
--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,6 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>{{ $title ?? 'Laravel' }}</title>
+<script src="https://cdn.jsdelivr.net/npm/frappe-charts@1.2.4/dist/frappe-charts.min.iife.js"></script>
 
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
